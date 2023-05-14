011cad5c7eedad0962853ae89db62c374b8f9aea
mominashfak786 <ashfakmomin2001@gmail.com>
2023-05-15 01:19:37 +0530

    done

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -101,6 +101,14 @@ const Dashboard = () => {
           data-drawer-hide="drawer-navigation"
           aria-controls="drawer-navigation"
           class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+          onPointerDown={() => {
+            const drawer = document.getElementById("drawer-navigation");
+            if (drawer.classList.contains("-translate-x-full")) {
+              drawer.classList.remove("-translate-x-full");
+            } else {
+              drawer.classList.add("-translate-x-full");
+            }
+          }}
         >
           <svg
             aria-hidden="true"
